f95918df4975c6515f4628824491d70c03d4e954
konkrotte <konkrotte@posteo.net>
2024-12-07 05:14:59 +0100

    fdkjsdkjf

--- flake.nix
+++ flake.nix
@@ -9,11 +9,7 @@
       legacyPackages = eachSystem (system:
         import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
       packages = eachSystem (system:
-        let pkgs = self.legacyPackages.${system};
-        in {
-          default =
-            pkgs.beetcamp; # Replace 'hello' with your actual default package
-          inherit (pkgs) beetcamp; # Add other packages you want to expose
-        });
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v)
+        self.legacyPackages.${system});
     };
 }
